fe1994fe12ef88e7a0535bf81a2af2e027ab3f26
Jamkris <dltmdgus1412@gmail.com>
2024-01-17 00:39:35 +0900

    CHORE :: 배포 URL 수정

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: 'http://localhost:3000/auth/google/callback',
+			callbackURL: 'https://port-0-omoking-1efqtf2dlrgiecc3.sel5.cloudtype.app/auth/google/callback',
 			passReqToCallback: true,
 		},
 		async (request, accessToken, refreshToken, profile, done) => {
