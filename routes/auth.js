86bb7eee4a306b4857868cbb42784a359e930995
Jamkris <dltmdgus1412@gmail.com>
2024-01-22 09:00:46 +0900

    CHORE :: Auth Callback 주소 변경

--- routes/auth.js
+++ routes/auth.js
@@ -23,8 +23,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			// callbackURL: 'https://omoking.jamkris.kro.kr/auth/google/callback'
-			callbackURL: 'http://localhost:3000/auth/google/callback'
+			callbackURL: process.env.CALLBACK
 		},
 		async (accessToken, refreshToken, profile, done) => {
 			try {
